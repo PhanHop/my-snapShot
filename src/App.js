05e20bda3aacb9d02c8799736cf939e9e3d71d84
PHANHOP-PC\admin <phanhop24022001@gmail.com>
2022-05-24 20:53:56 +0700

    B3

--- src/App.js
+++ src/App.js
@@ -5,6 +5,13 @@ import {BrowserRouter, Route, Routes} from 'react-router-dom'
 import Header from './components/Header';
 import Item from './components/Item';
 function App() {
+  const str = ["10000","00010"];
+  const length = str[0].length;
+  var result = '';
+  (function(){
+    var a=b=5
+  }){};
+  console.log(result);
   return (
     <PhotoContextProvider>
       <BrowserRouter >
--- src/Context/PhotoContext.js
+++ src/Context/PhotoContext.js
@@ -3,23 +3,30 @@ import { apiKey } from "../api/config";
 import { createContext, useState } from "react";
 import axios from 'axios';
 export const PhotoContext = createContext();
-const PhotoContextProvider = props=>{
+const PhotoContextProvider = props => {
     const [image, setImage] = useState([]);
     const [loading, setLoading] = useState(true);
-     
-    const runSearch = query=>{
-        const axi = axios. get(`https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=${apiKey}&tags=${query}&per_page=24&format=json&nojsoncallback=1`);
-        axi.then(response =>{
+    const [dataAPi, setDataApi] = useState([]);
+
+    const runSearch = query => {
+        const axi = axios.get(`https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=${apiKey}&tags=${query}&per_page=24&format=json&nojsoncallback=1`);
+        axi.then(response => {
             setImage(response.data.photos.photo);
             setLoading(false);
         })
-        .catch(error=>{
-            console.log(`Error: ${error}`)
-        })
-        console.log(axi.data);
+            .catch(error => {
+                console.log(`Error: ${error}`)
+            })
+            const api = axios.get('https://api.themoviedb.org/3/tv/63351?api_key=0a36bfae1984e2577ce56acd54f7a2f3');
+            api.then(response => {
+                console.log(response.data.results)
+                
+            })
+      
     }
+
     return (
-        <PhotoContext.Provider value={{image, loading, runSearch}}>
+        <PhotoContext.Provider value={{ image, loading, runSearch }}>
             {props.children}
         </PhotoContext.Provider>
     )
